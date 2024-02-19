4ada2d3221ad30005fd31cbb4b312236f341927a
rdb03 <rdb03@mail.ru>
2024-02-19 17:45:10 +0600

    Настроил проект для начала работы

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from "mongoose";
+import config from "./config";
+
+const app = express();
+const port = 8000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+const run = async () => {
+    await mongoose.connect(config.mongoose.db);
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    })
+};
+
+void run();
